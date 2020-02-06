37b798c384e2b34afb542051203ca4e184d2c595
Phillip Youn <phillipyoun@gmail.com>
2020-02-06 10:15:40 -0800

    ES6 for sprint 1 complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.index = 0;
+  	this.deleteIndex = 0;
+  	this.queueStorage = {};
+  	this.temp;
+  	this.difference;
+  }
+
+  enqueue(value){
+  	this.queueStorage[this.index] = value;
+  	this.index++;
+  }
+
+  dequeue(){
+  	this.temp = this.queueStorage[this.deleteIndex];
+  	delete this.queueStorage[this.deleteIndex];
+  	this.deleteIndex++;
+  	return this.temp;
+  }
+
+  size(){
+  	this.difference = this.index - this.deleteIndex;
+  	if (this.difference < 0){
+  		this.difference = 0;
+  	}
+  	return this.difference;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,26 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(obj) {
+  	this.index = 0;
+  	this.stackStorage = {};
+  }
+
+  push(value){
+  		this.stackStorage[this.index] = value;
+		this.index++;
+  }
+
+  pop(){
+  	delete this.stackStorage[this.index];
+  	if (this.index > 0){
+  		this.index--;
+  	}
+  	return this.stackStorage[this.index];
+  }
+
+  size(obj){
+  	return this.index;
   }
 
 }
