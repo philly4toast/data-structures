f12dccc384aaad34888b1dcb8815b9690b2a64a3
Phillip Youn <phillipyoun@gmail.com>
2020-02-05 23:10:45 -0800

    Write functional-shared revisions of queue/stack
    Having studied more on the different types of instantiation, things are
    becomming a lot easier.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
+	var queueObject = {};
+	queueObject.index = 0;
+	queueObject.deleteIndex = 0;
+	_.extend(queueObject, queueMethods)
+	return queueObject;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+var queueStorage = {};
+
+var queueMethods = {
+	enqueue: 	function(value){
+					queueStorage[this.index] = value;
+					this.index++;
+				},
+	dequeue:  	function(){
+					var temp;
+					temp = queueStorage[this.deleteIndex];
+					delete queueStorage[this.deleteIndex];
+					this.deleteIndex++;
+					return temp;
+				},
+	size: 		function(){
+					var difference = this.index - this.deleteIndex;
+					if (difference < 0){
+						difference = 0;
+					}
+					return difference;
+				}
+
+
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
+var objectInstance = {};
+objectInstance.index = 0;
+_.extend(objectInstance, stackMethods)
+return objectInstance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
+var stackStorage ={};
+var stackMethods = {
+	push: 	function(value){
+				stackStorage[this.index] = value;
+				this.index++;
+			},
+	pop: 	function(){
+				delete stackStorage[this.index];
+				if (this.index > 0){
+					this.index--;
+				}
+				return stackStorage[this.index];
+			},
+	size: 	function(obj){
+				return this.index;
+			}
 
-var stackMethods = {};
+};
 
 
